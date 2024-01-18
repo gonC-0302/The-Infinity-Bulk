c2e1bd93fdca61a1d551ab6c5741678728af099f
shingogoto-t8s <shingo.goto@t8s.co.jp>
2024-01-18 18:22:55 +0900

    主人公のアニメーションテクスチャ追加

--- Assets/SnadBox/goto/@TheInfinityBulk/Dungeon/Scripts/Player/PlayerMovement.cs
+++ Assets/SnadBox/goto/@TheInfinityBulk/Dungeon/Scripts/Player/PlayerMovement.cs
@@ -11,39 +11,63 @@ namespace Dungeon
         private Vector3 _scale = Vector3.one;
         private Vector2 _direction;
         [SerializeField] private bool _isJoyStick;
-        Rigidbody2D rigidbody2d;
+        Rigidbody2D _rigidbody2d;
+        private Animator _anim;
        
         void Start()
         {
-            rigidbody2d = GetComponent<Rigidbody2D>();
+            _rigidbody2d = GetComponent<Rigidbody2D>();
+            _anim = GetComponent<Animator>();
         }
-
         void Update()
         {
+            // TODO: 攻撃アニメーションが終わるまではボタンを押せないようにする
+            if (Input.GetKeyDown(KeyCode.Space)) _anim.SetTrigger("Attack");
+
+            _direction = ReceiveMoveInput();
+            if (_direction == Vector2.zero)
+            {
+                _anim.SetBool("IsWalking", false);
+                return;
+            }
+            else
+            {
+                _anim.SetBool("IsWalking", true);
+            }
+            ChangeDirection();
+        }
+        private Vector3 ReceiveMoveInput()
+        {
+            Vector3 direction = Vector3.zero;
             if (_isJoyStick)
             {
-                _direction = Vector2.right * variableJoystick.Horizontal + Vector2.up * variableJoystick.Vertical;
+                direction = Vector2.right * variableJoystick.Horizontal + Vector2.up * variableJoystick.Vertical;
             }
             else
             {
                 float horizontal = Input.GetAxisRaw("Horizontal");
                 float vertical = Input.GetAxisRaw("Vertical");
-                _direction = new Vector2(horizontal, vertical);
+                direction = new Vector2(horizontal, vertical);
             }
-            if (_direction.x == 0) return;
-
-            if (_direction.x >= 0) _scale.x = 1;
-            else _scale.x = -1;
-            transform.localScale = _scale;
-
+            return direction;
         }
-
+        private void ChangeDirection()
+        {
+            if (_direction.x == 0) return;
+            if (_direction.x >= 0) _scale.x = -1;
+            else _scale.x = 1;
+            transform.localScale = _scale;
+        }
         void FixedUpdate()
         {
-            Vector2 position = rigidbody2d.position;
+            Move();
+        }
+        private void Move()
+        {
+            Vector2 position = _rigidbody2d.position;
             position.x = position.x + _speed * _direction.x * Time.deltaTime;
             position.y = position.y + _speed * _direction.y * Time.deltaTime;
-            rigidbody2d.MovePosition(position);
+            _rigidbody2d.MovePosition(position);
         }
     }
 }
